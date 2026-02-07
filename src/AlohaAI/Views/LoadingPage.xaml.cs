2eb9998a2e54af3a4c6f84b9f53c47c079c0b696
Jakub Florkowski <kubaflo123@gmail.com>
2026-02-07 23:12:21 +0100

    Refine HomePage layout: compact cards, horizontal header, add descriptions and tag pills

--- src/AlohaAI/Views/LoadingPage.xaml.cs
+++ src/AlohaAI/Views/LoadingPage.xaml.cs
@@ -18,22 +18,21 @@ public partial class LoadingPage : ContentPage
 
         try
         {
+            await Task.Delay(500); // Brief splash display
             await _progressService.InitializeAsync();
-            var onboarded = await _progressService.GetSettingAsync("onboarding_completed");
-
-            if (Application.Current?.Windows.Count > 0)
-            {
-                // Mark onboarding as done so we always go to main shell
-                if (onboarded != "true")
-                    await _progressService.SaveSettingAsync("onboarding_completed", "true");
-                Application.Current.Windows[0].Page = new AppShell();
-            }
+            await _progressService.SaveSettingAsync("onboarding_completed", "true");
         }
         catch (Exception ex)
         {
             System.Diagnostics.Debug.WriteLine($"Loading error: {ex.Message}");
-            if (Application.Current?.Windows.Count > 0)
-                Application.Current.Windows[0].Page = new AppShell();
+        }
+        finally
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                if (Application.Current?.Windows.Count > 0)
+                    Application.Current.Windows[0].Page = new AppShell();
+            });
         }
     }
 }
